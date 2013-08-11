14ed01c9d57697c48258a1c919e5270e6694a9d3
Blair Anderson <blair81@gmail.com>
2013-08-11 14:59:21 -0600

    updated some specs

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,19 +1,34 @@
 require 'spec_helper'
 
 describe 'User Auth' do 
+  username = "blair"
+  password = "password"
+  let!(:valid_user) { User.create!(username: username, password: password)}
+
+  before :each do
+    expect(valid_user).to be_valid
+  end
+
   describe 'valid credentials' do 
-    let(:valid_user) { User.create!(username: "blair", password: "password")}
-    before :each do
-      expect(valid_user).to be_valid
-    end
-
-    it 'shows login on home screen' do 
+    it 'user can login on home screen' do 
       visit root_path
-      expect( page ).to have_link "Login"
-    end
+      expect( page ).to have_field "username"
+      expect( page ).to have_field "password"
 
+      fill_in "username", with: valid_user.username
+      fill_in "password", with: password
+      click_on "Login"
+      expect( page ).to have_content "Logout"
+    end
   end
 
   describe 'invalid credentials' do 
+    it 'user cannot login' do 
+      visit root_path
+      fill_in "username", with: valid_user.username
+      fill_in "password", with: password + "654"
+      click_on "Login"
+      expect( page ).to have_content "Login failed"
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,29 @@ require 'spec_helper'
 
 describe User do
   describe 'validations' do
-    it { should validate_presence_of :username }
-    it { should validate_uniqueness_of :username }
+    let(:user){User.create!(username: "blair", password: "password")}
+    before :each do 
+      expect(user).to be_valid
+    end
+
+    it 'should be invalid without a username' do 
+      user.username = nil
+      expect(user).to be_invalid
+    end
+
+    it 'should not allow duplicate input' do 
+      invalid_user = User.create(username: user.username, password: "password")
+      expect(invalid_user).to be_invalid
+    end
+
+    it 'should be case insensitive' do 
+      invalid_user = User.create(username: user.username.upcase, password: "password")
+      expect(invalid_user).to be_invalid
+    end
+
+    it 'password length should be greater than 3 chars' do 
+      invalid_user = User.create(username: "invalid-user", password: "12")
+      expect(invalid_user).to be_invalid
+    end
   end
 end
