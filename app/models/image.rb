53e81b2016ae27e527b740d222b49a4b756a5480
Blair Anderson <blair81@gmail.com>
2013-08-13 23:32:01 -0600

    add processing methods to images

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,8 @@ class Image < ActiveRecord::Base
   belongs_to :post
   validates_presence_of :post_id
   validates_presence_of :url
+
+  def url_size(number = 500)
+    url + "/convert?w=#{number}&h=#{number}&fit=max"
+  end
 end
